e7d7d0e7d957a689fd0cb399a9e4cdedd2b90b68
MxunJenson <mxeng0869@gmail.com>
2026-04-30 04:50:34 +0800

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print(data)
+
+    if data.get("action") == "buy":
+        print("BUY EXECUTED")
+
+    if data.get("action") == "sell":
+        print("SELL EXECUTED")
+
+    return "ok"
+
+app.run(host='0.0.0.0', port=10000)
